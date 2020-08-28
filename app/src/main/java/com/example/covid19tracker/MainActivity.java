c978c8d463a8afba026ab3dbf2d3a01176f06453
hapn12345 <hapham351999@gmail.com>
2020-08-29 01:42:31 +0700

    Main UI design

--- app/src/main/java/com/example/covid19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.covid19tracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    //UI Views
+    private TextView titleTv;
+    private ImageButton refreshBtn;
+    private BottomNavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // init UI views
+        titleTv = findViewById(R.id.titleTv);
+        refreshBtn = findViewById(R.id.refreshBtn);
+        navigationView = findViewById(R.id.navigationView);
+        //refresh button view
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        //handle bottom nav item clicks
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                //loadhome data
+                return true;
+            case R.id.nav_stats:
+                //loadstat data
+                return true;
+        }
+        return false;
     }
 }
