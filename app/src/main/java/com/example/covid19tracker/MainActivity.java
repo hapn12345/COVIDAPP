a2469220e86c01884bfb5dc60e92b3958fb67df0
hapn12345 <hapham351999@gmail.com>
2020-09-03 23:09:01 +0700

    fragments transaction

--- app/src/main/java/com/example/covid19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.covid19tracker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -12,11 +14,17 @@ import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
     //UI Views
     private TextView titleTv;
     private ImageButton refreshBtn;
     private BottomNavigationView navigationView;
+    //Fragments
+    private Fragment homeFragment,statsFragments;
+    private Fragment activeFragment;
+    private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         titleTv = findViewById(R.id.titleTv);
         refreshBtn = findViewById(R.id.refreshBtn);
         navigationView = findViewById(R.id.navigationView);
+        initFragments();
         //refresh button view
         refreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,14 +45,42 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         navigationView.setOnNavigationItemSelectedListener(this);
     }
 
+    private void initFragments() {
+        homeFragment = new HomeFragment();
+        statsFragments = new StatsFragment();
+
+        fragmentManager = getSupportFragmentManager();
+        activeFragment = homeFragment;
+
+        fragmentManager.beginTransaction()
+                .add(R.id.frame, homeFragment, "homeFragment")
+                .commit();
+        fragmentManager.beginTransaction()
+                .add(R.id.frame, statsFragments, "statsFragment")
+                .hide(statsFragments)
+                .commit();
+    }
+    private void loadHomeFragment(){
+        titleTv.setText("Home");
+        fragmentManager.beginTransaction().hide(activeFragment).show(homeFragment).commit();
+        activeFragment = homeFragment;
+    } private void loadStatsFragment(){
+        titleTv.setText("Stats");
+        fragmentManager.beginTransaction().hide(activeFragment).show(statsFragments).commit();
+        activeFragment = statsFragments;
+    }
+
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         //handle bottom nav item clicks
         switch (item.getItemId()){
             case R.id.nav_home:
+                loadHomeFragment();
                 //loadhome data
                 return true;
             case R.id.nav_stats:
+                loadStatsFragment();
                 //loadstat data
                 return true;
         }
